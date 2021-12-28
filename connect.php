f3e1eefd71988b044cf570cea93c210261f6e51b
victory-subbu <71482105+victory-subbu@users.noreply.github.com>
2021-12-28 12:45:20 +0530

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,36 @@
+
+
+
+
+<?php
+echo "hello";
+/*$servername="localhost";
+$username="root";
+$password="";
+$database_name="miniproject";
+$conn=mysqli_connect($servername,$username,$password,database_name);
+if(!$conn){
+    die('connection failed'. mysqli_connect_error);
+}
+
+echo "hello world";
+
+
+if(isset($_POST['save'])){
+  header('highestmain.html?status=success');
+  $name=$_POST['name'];
+  $username=$_POST['username'];
+  $collegename=$_POST['collegename'];
+  $password=$_POST['password'];
+  $sql_query="INSERT INTO signup (name,username,collegename,password) values('$name','$username','$collegename','$password')";
+  if(mysqli_query($conn,$sql_query)){
+      echo "succ";
+  }
+    else{
+        echo "fail";
+  
+  }
+  mysqli-close($conn);
+*/
+
+?>
